0a16dea9c9e6f8c31d09cd0a9b48ef4bc69daeda
Polyakov Artur <ar.polyakov@astondevs.ru>
2024-03-11 10:57:58 +0300

    init

---
+++ src/test/java/ru/aston/paymentparsingdesktop/PaymentParsingDesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aston.paymentparsingdesktop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentParsingDesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
