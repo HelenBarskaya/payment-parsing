bb93b721a84349314b5c63685552fc2da86c2a5c
Polyakov Artur <ar.polyakov@astondevs.ru>
2024-03-11 11:10:31 +0300

    add packages

--- src/main/java/ru/aston/PaymentParsingApplication.java
+++ src/main/java/ru/aston/PaymentParsingApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentParsingDesktopApplication {
+public class PaymentParsingApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentParsingDesktopApplication.class, args);
+		SpringApplication.run(PaymentParsingApplication.class, args);
 	}
 
 }
